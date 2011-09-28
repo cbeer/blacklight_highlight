c2f14c245774a281662768414660f6d91d87a771
Chris Beer <chris_beer@wgbh.org>
2011-09-28 17:11:59 -0400

    fix routes for rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
-
 # We want to add a new collection action to Catalog, without over-writing
 # what's already there. This SEEMS to do it. 
-ActionController::Routing::Routes.draw do |map|  
+BlacklightHighlight::Engine.routes.draw do   
 end
 

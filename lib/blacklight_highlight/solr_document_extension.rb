4da8aef9374419a6db2fb849061ea55e48adc008
Chris Beer <chris@cbeer.info>
2011-12-22 16:38:13 -0500

    provide a highlight field whitelist for selecting highlighting

--- lib/blacklight_highlight/solr_document_extension.rb
+++ lib/blacklight_highlight/solr_document_extension.rb
@@ -1,28 +1,57 @@
 module BlacklightHighlight
   module SolrDocumentExtension
-    def self.included some_class
-      some_class.after_initialize do
+    extend ActiveSupport::Concern
+
+    included do 
+      after_initialize do
         solr_response.send(:extend, RSolr::Ext::Response::Highlight) unless solr_response.is_a? RSolr::Ext::Response::Highlight
       end
     end
 
+    module ClassMethods
+      ##
+      # Provide a class-level method to white-list highlighted fields
+      # By default, it will highlight every field for which there's a highlight result
+      def highlight
+        @highlight ||= []
+      end
+    end
+
+    ## 
+    # Override RSolr::Doc methods with highlighting equivalents
     def [] key
-      return super(key) if key == self.class.unique_key
-      return highlight(key) if highlight_fields.key?(key) and not highlight(key).blank?
-
+      return highlight(key) if highlight?(key)
       super(key)
     end
 
+    ## 
+    # Override RSolr::Doc methods with highlighting equivalents
     def key? key
-      return super(key) if key == self.class.unique_key
-      return true if highlight_fields.key?(key)
-    
+      return true if highlight?(key)
       super(key)
     end
 
-    def highlight key = nil
-      return if key == self.class.unique_key
+    ##
+    # Get the (html-safe) highlight values for a field
+    # @param [String] key
+    def highlight key
+      return unless highlight? key
       highlight_fields[key].map { |x| x.html_safe }
+    end
+    
+    ##
+    # Does the field have highlighting values?
+    # @param [String] key
+    def highlight? key
+      return false if key.nil? or key == self.class.unique_key
+
+      if self.class.highlight.include? key.to_sym
+        return true
+      elsif !self.class.highlight.empty?
+        return false
+      end
+
+     !highlight_fields[key].blank?
     end
 
     def highlight_fields

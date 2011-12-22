25c353a7d164a74fb5c1b6d4881416e2929ccd25
Chris Beer <chris@cbeer.info>
2011-12-22 16:38:59 -0500

    remove unused highlight_config

--- lib/blacklight_highlight/controller_extension.rb
+++ lib/blacklight_highlight/controller_extension.rb
@@ -2,17 +2,8 @@
 # Blacklight::SolrHelper. 
 module BlacklightHighlight::ControllerExtension
   def self.included(some_class)
-    some_class.helper_method :highlight_config
     some_class.helper BlacklightHighlightHelper
     some_class.send(:include, BlacklightHighlight::SolrHelperExtension)
-  end
-
-  # Uses Blacklight.config, needs to be modified when
-  # that changes to be controller-based. This is the only method
-  # in this plugin that accesses Blacklight.config, single point
-  # of contact. 
-  def highlight_config
-    Blacklight.config[:highlight] || {}
   end
 end
 
